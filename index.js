ad0561815664805813ccfe369c673c880e9d94bf
laleth <trlaleth@gmail.com>
2024-05-01 12:33:11 +0530

    added new frontend url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 5000
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: "https://food-order-frontend-neon.vercel.app", 
     credentials: true,
   }))
 
